d3271595fcae6325eb2210d98443e6e32e561bd4
hassen mohamed <hassenmohamed.92@gmail.com>
2024-03-21 14:46:18 +0100

    first commit

---
+++ Screens/NFTDetails.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const NFTDetails = () => {
+  return (
+    <View>
+      <Text>NFTDetails</Text>
+    </View>
+  );
+};
+
+export default NFTDetails;
